9a7a4aad976f45f543e50b9a200c7c6955c8d4c4
Meredith <bitcababy@gmail.com>
2013-01-25 11:57:40 -0500

    Changed to use enable_locking_with :symbol

--- lib/mongoid/locker.rb
+++ lib/mongoid/locker.rb
@@ -6,48 +6,71 @@ module Mongoid
 
     included do
       require 'state_machine'
-
-      attr_accessor :locked_at
-
-      class_attribute :timeout
-      self.timeout = 30
-
-      state_machine :state, :initial => :unlocked do
-        before_transition :locked => :locked, :if => :can_relock?, :do => :reset_lock
-        before_transition :unlocked => :locked, :do => :set_lock
-        before_transition :locked => :unlocked, :do => :clear_lock
- 
-         event :lock do
-           transition :unlocked => :locked
-           transition :locked => :locked, :if => :can_relock?
-           transition :locked => same, :do => :raise_lock_error
-         end
-     
-         event :unlock do
-           transition :locked => :unlocked, :do => :clear_lock
-           transition :unlocked => same # Should there be an error?
-         end             
-       end
-    end
-
-    def set_lock
-      @locked_at = Time.now
-    end
-
-    def clear_lock
-      @locked_at = nil
-    end
-
-    def can_relock?
-      return self.locked_at ? ((Time.now - self.locked_at ) > timeout) : true
-    end
-      
-    def raise_lock_error
-      raise Mongoid::Locker::IsLockeredException("is already locked")
-    end
   
-    class IsLockeredException < Exception
     end
+
+    module ClassMethods
+      def enable_locking_with fn
+        self.class_attribute :timeout
+        self.timeout = 5*60
+ 
+        field fn, type: Time, default: nil
+
+        define_method :set_lock do 
+          self[fn] = Time.now.localtime
+        end
+
+        define_method :clear_lock do
+          self[fn] = nil
+        end
     
+        define_method :update_time do
+          self[fn] = Time.now.localtime
+        end
+
+        define_method :timed_out? do
+          return self[fn] ? ((Time.now.localtime - self.locked_at ) > self.class.timeout) : true
+        end
+      
+        define_method :raise_lock_error do
+          raise Mongoid::Locker::IsLockeredException("is already locked")
+        end
+
+        state_machine :state, :initial => :unlocked do
+          before_transition any - :locked => :locked, :do => :set_lock
+          before_transition :locked => :unlocked, :do => :clear_lock
+          before_transition :on => :relock, :do => :set_lock
+    
+          event :lock do
+            transition :unlocked => :locked
+            transition :locked => :locked, :if => :timed_out?
+            transition :locked => same, :do => :raise_lock_error
+          end
+     
+          event :unlock do
+            transition :locked => :unlocked
+            transition :unlocked => same
+          end
+         
+          event :relock do
+            transition :locked => :locked, :do => :update_time
+          end
+         
+          event :break_lock do
+            transition any => :unlocked
+          end
+         
+          state :locked do
+            validates_presence_of :locked_at
+          end
+
+        end # state_machine
+      end # lock_using
+ 
+      class IsLockeredException < Exception
+      end
+    end #ClassMethods
+
+     
   end
 end
